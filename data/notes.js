d9c2e05ca21b31cf369522e11325a9578d806bc8
KunjDesai96 <36724761+KunjDesai96@users.noreply.github.com>
2019-11-11 23:32:47 -0500

    Adding Note

---
+++ data/notes.js
@@ -0,0 +1,29 @@
+const mongoCollections = require("../config/mongoCollections");
+const notesData = mongoCollections.notes;
+
+const createNotes = async function createNotes(userID, title, content) {
+    if (!userID) throw "No user provided."
+    if (!title) throw "No title provided."
+    if (!content) throw "No content provided."
+
+    const notesCollection = await notesData();
+    const dateTime = new Date().toLocaleString('en-US');
+    const noteInfo = {
+        userID: userID,
+        title: title,
+        content: content,
+        latitute: 0,
+        longitute: 0,
+        radius: 10,
+        note_createdAt: dateTime
+    }
+
+    const insertInfo = await notesCollection.insertOne(noteInfo);
+
+    if (insertInfo.insertedCount === 0) throw "Could not add user";
+}
+
+const findNote = async function findNote()
+module.exports = {
+    createNotes
+}
