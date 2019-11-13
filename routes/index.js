0a64b9a94951e168f86be5242e9c9044531f8af1
SunilSwarna <sunil.k14@iiits.in>
2019-11-13 00:03:59 -0500

    CSS Updation for Location

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
+const locationRoutes =require('./location')
 
 const constructorMethod = app => { 
 
 
-    app.get("/", async (req, res) => {
-      try 
-      {
-          res.render("location", {});
-      }
-      catch(e) 
-      {    
-          res.status(404).json({"error": "Couldn't load page"});    
-      }
-  })
+    app.use("/", locationRoutes) 
    
     app.use("*", (req, res) => {
       res.status(404).json({"error": "Please Enter a Valid URL"});
