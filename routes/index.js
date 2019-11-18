fe5c525b00686636179d277e65b7071c41ceb031
KunjDesai96 <36724761+KunjDesai96@users.noreply.github.com>
2019-11-17 23:57:56 -0500

    Creating views and routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
-const locationRoutes =require('./location')
+const homeRoutes = require('./home')
+const friendsRoutes = require("./friends");
+const addPostRoutes = require("./addPost");
 
-const constructorMethod = app => { 
+const constructorMethod = app => {
 
 
-    app.use("/", locationRoutes) 
-   
+    app.use("/", homeRoutes)
+    app.use("/friends", friendsRoutes)
+    app.use("/addPost", addPostRoutes)
     app.use("*", (req, res) => {
-      res.status(404).json({"error": "Please Enter a Valid URL"});
+        res.status(404).json({ "error": "Please Enter a Valid URL" });
     });
-  };
-  
-  module.exports = constructorMethod;
+};
+
+module.exports = constructorMethod;
