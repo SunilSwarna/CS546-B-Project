879a568daec0df03f565d910ed9b606c6de53538
SunilSwarna <sunil.k14@iiits.in>
2019-12-14 03:44:37 -0500

    Logour Route & tottaly

--- public/js/friends.js
+++ public/js/friends.js
@@ -34,21 +34,21 @@ submit.addEventListener("click", (event) => {
                 if(person.status && person.status ==2){
                     list_friends.innerHTML += '<li style="border: none" class="list-group-item d-flex justify-content-between">' +
                 `<span class="mt-2">${person.firstName+" "+ person.lastName}</span>`+
-                    "<button style='margin-left:15px' class='btn btn-primary btn-sm' disabled>"+
+                    "<button style='margin-left:15px;color: rgb(255, 255, 255);background-color: #0012e8;' class='btn btn-primary btn-sm' disabled>"+
                     'Pending Request</button>' +
                     '</li>';
                 }
                 else if(person.status && person.status ==1){
                     list_friends.innerHTML += '<li style="border: none" class="list-group-item d-flex justify-content-between">' +
                 `<span class="mt-2">${person.firstName+" "+ person.lastName}</span>`+
-                    "<button style='margin-left:15px;color: rgb(255, 255, 255);background-color: #007bc8;' class='btn btn-primary btn-sm' disabled>"+
+                    "<button style='margin-left:15px;color: rgb(255, 255, 255);background-color: #0012e8;' class='btn btn-primary btn-sm' disabled>"+
                     'Already Friend</button>' +
                     '</li>';
                 }
                 else{
                     list_friends.innerHTML += '<li style="border: none" class="list-group-item d-flex justify-content-between">' +
                 `<span class="mt-2">${person.firstName+" "+ person.lastName}</span>`+
-                    "<button style='margin-left:15px;color: rgb(255, 255, 255);background-color: #007bc8;' class='btn btn-primary btn-sm' onClick='add_friend(this,\""+String(person.friendID)+"\" )'>"+
+                    "<button style='margin-left:15px;color: rgb(255, 255, 255);' class='btn btn-primary btn-sm' onClick='add_friend(this,\""+String(person.friendID)+"\" )'>"+
                     'Add Friend</button>' +
                     '</li>';
                 }
